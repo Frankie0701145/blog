76af62683f63420ce27c9808cae21cb5885d2d07
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-28 17:55:37 +0300

    Add the app-materal-module

---
+++ src/app/modules/angular-material/app-material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatSidenavModule, 
+  MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule,
+  MatInputModule
+} from '@angular/material'
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule
+  ]
+})
+export class AngularMaterialModule { }
