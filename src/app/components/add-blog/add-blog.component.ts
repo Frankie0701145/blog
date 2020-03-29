b130db031aa150063283f75bd2c07b6002694c82
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-29 18:05:20 +0300

    Setup the endpoint to add a blog

--- src/app/components/add-blog/add-blog.component.ts
+++ src/app/components/add-blog/add-blog.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-blog.component.sass']
 })
 export class AddBlogComponent implements OnInit {
-
+  fileData: File = null;
+  previewUrl:any = null;
+  // fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fileProgress(event: any){
+    this.fileData = <File> event.target.files[0];
+    this.preview()
+  }
+  preview(){
+    let reader = new FileReader()
+    reader.readAsDataURL(this.fileData);
+    reader.onload = (_event) => { 
+      this.previewUrl = reader.result; 
+    }
   }
 
 }
