c5b25fa5234c8205d7df4677148d0803d3e9afe0
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-29 13:04:47 +0300

    Add the navigation to the add-blog component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {LoginComponent} from './components/login/login.component';
+import { AddBlogComponent } from './components/add-blog/add-blog.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {path: 'add_blog', component: AddBlogComponent}
 ];
 
 @NgModule({
