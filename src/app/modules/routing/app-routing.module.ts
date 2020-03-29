d47c5c528184fb5d3d04301e2014e12aa523e4aa
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-29 18:33:46 +0300

    Add the routing module into the module folder

--- src/app/modules/routing/app-routing.module.ts
+++ src/app/modules/routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import { AddBlogComponent } from './components/add-blog/add-blog.component';
-import {BlogsComponent} from './components/blogs/blogs.component'
+import {LoginComponent} from '../../components/login/login.component';
+import { AddBlogComponent } from '../../components/add-blog/add-blog.component';
+import {BlogsComponent} from '../../components/blogs/blogs.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
