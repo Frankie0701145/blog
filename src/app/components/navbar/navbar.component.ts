100dbf1b3ea123a8c92f288b6679606349276eab
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-31 00:03:35 +0300

    Change the links based on the loggedIn state

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {selectLoggedIn} from '../../store/selectors/loggedIn.selector'
+import { Store, select } from '@ngrx/store';
+import { IAppState } from 'src/app/store/state/app.state';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent implements OnInit {
+  loggedIn: boolean
 
-  constructor() { }
+  constructor(
+    private _store: Store<IAppState>
+  ) {
+    this._store.pipe(select(selectLoggedIn)).subscribe((loggedIn)=>{
+        this.loggedIn =loggedIn;
+    });
+  }
 
   ngOnInit() {
   }
