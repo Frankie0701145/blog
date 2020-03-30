ad743e3d876aa580e5cb041e1bbfdd0f27ed4145
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-30 12:28:24 +0300

    Create the blog.selector.ts

---
+++ src/app/store/selectors/blog.selector.ts
@@ -0,0 +1,10 @@
+import {createSelector} from '@ngrx/store';
+import { IAppState } from '../state/app.state';
+import { IBlogsState } from '../state/blogs.state';
+
+const selectBlogs = (state: IAppState)=> state.blogs;
+
+export const selectBlogList = createSelector(
+    selectBlogs,
+    (state: IBlogsState)=> state.blogs
+); 
