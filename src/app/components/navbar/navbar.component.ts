825dead51f8066c47ec2eafc04785955220fbbd4
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-31 00:11:47 +0300

    Setup the logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {selectLoggedIn} from '../../store/selectors/loggedIn.selector'
 import { Store, select } from '@ngrx/store';
 import { IAppState } from 'src/app/store/state/app.state';
-
+import { LogoutSuccess } from 'src/app/store/actions/loggedIn.actions';
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -12,7 +13,8 @@ export class NavbarComponent implements OnInit {
   loggedIn: boolean
 
   constructor(
-    private _store: Store<IAppState>
+    private _store: Store<IAppState>,
+    private route: Router
   ) {
     this._store.pipe(select(selectLoggedIn)).subscribe((loggedIn)=>{
         this.loggedIn =loggedIn;
@@ -20,6 +22,10 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  logout(){
+    this._store.dispatch(new LogoutSuccess())
+    this.route.navigate(['/blogs'])
   }
 
 }
