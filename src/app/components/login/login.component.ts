8c56fd93eed0a76224b6e3d95d53ab329a458aab
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-30 22:44:43 +0300

    Setup the login functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import { IAppState } from 'src/app/store/state/app.state';
+import { Store } from '@ngrx/store';
+import { LoginSuccess } from 'src/app/store/actions/loggedIn.actions';
+import {Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,12 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-  
+  blogForm
   hide: boolean= true;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private _store: Store<IAppState>,
+    private route: Router
+  ) { 
+    this.blogForm = this.formBuilder.group({
+      email: "",
+      password: ""
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(userData){
+      let payload ={
+        accessToken: "asmcdaklsnmclamslcmkalmc"
+      }
+      this._store.dispatch(new LoginSuccess(payload));
+      this.route.navigate(['/blogs'])
   }
 
 }
