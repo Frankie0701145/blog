d8a8536f6354f432660f78cadee1ab1355d2333a
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-30 13:55:01 +0300

    Define the blog.service and import the HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './modules/routing/app-routing.module';
 import {AngularMaterialModule} from './modules/angular-material/app-material.module';
@@ -10,6 +11,7 @@ import {LoginComponent} from './components/login/login.component';
 import { AddBlogComponent } from './components/add-blog/add-blog.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { CommentsComponent } from './components/blogs/comments/comments.component';
+import {BlogService} from './services/blog.service'
 
 @NgModule({
   declarations: [
@@ -24,9 +26,10 @@ import { CommentsComponent } from './components/blogs/comments/comments.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BlogService],
   bootstrap: [AppComponent],
   entryComponents: [CommentsComponent]
 })
