3853183c2dcb6335cea4d17c9cf4583504dda621
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-01 02:11:39 +0300

    Add comments to that blogs.component

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -10,6 +10,8 @@ import {selectFilteredBlogList } from 'src/app/store/selectors/filteredBlogs.sel
 import {  RemoveComments } from 'src/app/store/actions/comment.actions';
 import { selectIsSearching } from 'src/app/store/selectors/isSearching.selector';
 import { selectLoggedIn } from 'src/app/store/selectors/loggedIn.selector';
+import { Observable } from 'rxjs';
+import { IBlog } from 'src/app/models/blog.interface';
 
 
 @Component({
@@ -18,32 +20,43 @@ import { selectLoggedIn } from 'src/app/store/selectors/loggedIn.selector';
   styleUrls: ['./blogs.component.sass']
 })
 export class BlogsComponent implements OnInit {
-  blogs$ = this._store.pipe(select(selectBlogList));
-  newBlogs$ = this._store.pipe(select(selectNewBlogList));
-  filteredBlog$ = this._store.pipe(select(selectFilteredBlogList ))
+  /**The blogs observable to retrieve the blogs from the state*/ 
+  blogs$: Observable<IBlog[]> = this._store.pipe(select(selectBlogList));
+  /**The newBlogs observable to retrieve the  newBlogs from the state*/
+  newBlogs$: Observable<IBlog[]> = this._store.pipe(select(selectNewBlogList));
+  /**The filteredBlogs observable to retrieve the filteredBlogs from the state*/
+  filteredBlog$: Observable<IBlog[]> = this._store.pipe(select(selectFilteredBlogList ))
+  /**Property to keep track when the user is searching */
   isSearching: boolean;
+  /**Property to check the loggedIn state*/
   loggedIn: boolean;
   constructor(
+    /**Inject the dialog from MatDialog*/
     private dialog: MatDialog,
+    /**injects the store*/
     private _store: Store<IAppState>
   ) {
+    /**Set the isSearching property to that of isSearching from the state*/
     this._store.pipe(select(selectIsSearching)).subscribe((isSearching)=>{
       console.log(isSearching)
       this.isSearching = isSearching
     })
+    /**Set the loggedIn property to that of the loggedIn from the state*/
     this._store.pipe(select(selectLoggedIn)).subscribe((loggedIn)=>{
       this.loggedIn = loggedIn
     })
   }
 
   ngOnInit() {
+    /**dispatch the GeBlogs action to fetch blogs*/
     this._store.dispatch(new GetBlogs());
  
   }
-
+  
+  /**Function to open the matDialog and initialize*/
   openCommentDialog(blogId: string){
     const dialogConfig = new MatDialogConfig();
-    // this._store.dispatch(new GetComments(blogId));
+
     dialogConfig.maxHeight = "500px";
     dialogConfig.maxWidth = "500px";
     dialogConfig.minHeight = "200px";
@@ -51,9 +64,10 @@ export class BlogsComponent implements OnInit {
     dialogConfig.data = {
         blogId: blogId
     };
-    // dialogConfig.width = "60%";
     let dialogRef = this.dialog.open(CommentsComponent, dialogConfig);
+
     dialogRef.afterClosed().subscribe((result)=>{
+        /**After the comment dialog closes dispatch the RemoveComments action to remove the comments*/
         this._store.dispatch(new RemoveComments())
     })
   }
