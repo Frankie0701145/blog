82aceb5076c54f5a5bedae1ed470543d1abf29c6
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-01 02:33:58 +0300

    Add comments to the edit-blog-component.

--- src/app/components/edit-blog/edit-blog.component.ts
+++ src/app/components/edit-blog/edit-blog.component.ts
@@ -4,7 +4,6 @@ import {FormBuilder} from '@angular/forms';
 import { IAppState } from 'src/app/store/state/app.state';
 import { selectBlogList } from 'src/app/store/selectors/blog.selector';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 import { GetBlogs, EditBlogSuccess } from 'src/app/store/actions/blog.actions';
 
 @Component({
@@ -13,33 +12,42 @@ import { GetBlogs, EditBlogSuccess } from 'src/app/store/actions/blog.actions';
   styleUrls: ['./edit-blog.component.sass']
 })
 export class EditBlogComponent implements OnInit {
-
+  /**To hold the file data of the image*/
   fileData: File = null;
+  /**for holding the preview url*/
   previewUrl:any = null;
+  /**to hold the form for the blog*/
   blogForm;
+  /**The blog*/
   blog;
+  /**The blogId*/
   blogId: string;
   
 
   constructor(
+    /**inject the formBuilder*/
     private formBuilder: FormBuilder,
+    /**inject the store*/
     private _store: Store<IAppState>,
+    /**inject the route*/
     private route: ActivatedRoute,
+    /**inject the router*/
     private router: Router,
   ) {
-
+    /**subscribe to the paramMap and retrieve the id*/
     this.route.paramMap.subscribe((params: ParamMap)=>{
       this.blogId = "" +params.get('id')
-      console.log(this.blogId)
     })
 
   }
 
+  /**monitor upload of image and set the preview*/
   fileProgress(event: any){
     this.fileData = <File> event.target.files[0];
     this.preview()
   }
 
+  /**function to set the previewUrl*/
   preview(){
     let reader = new FileReader()
     reader.readAsDataURL(this.fileData);
@@ -49,25 +57,31 @@ export class EditBlogComponent implements OnInit {
   }
 
   ngOnInit() {
+    /**fetch the blogs*/
     this._store.dispatch(new GetBlogs());
 
+    /**select the blogs from the state*/
     this._store.pipe(select(selectBlogList)).subscribe((blogs)=>{
- 
+      /**retrieve the blog that match the blogId*/
       let blogsResult = blogs.filter((blog)=>blog.id==this.blogId)
       let blog = blogsResult[0]
-
+      /**Build the form */
       this.blogForm = this.formBuilder.group({...blog});
+      /**Set the blog*/
       this.blog= blog;
     });
   }
 
+  /**Submit the editForm*/
   onSubmit(formValue){
-    console.log(formValue)
+    /**Create the data*/
     let data = {
       title: formValue.title,
       body: formValue.body
     }
+    /**Dispatch the action EditBlogSuccess passing the payload of the property to edit with the blogId*/
     this._store.dispatch(new EditBlogSuccess({blogId: this.blogId, blogProperty: data}));
+    /**navigate to the blogs*/
     this.router.navigate(['/blogs'])
   }
 
