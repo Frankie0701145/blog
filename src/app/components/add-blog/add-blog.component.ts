309dcfaa3dcc2ecf57750720197737c9eae240b3
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-01 01:59:23 +0300

    Add comments to the add-blog.component

--- src/app/components/add-blog/add-blog.component.ts
+++ src/app/components/add-blog/add-blog.component.ts
@@ -11,16 +11,18 @@ import { IAppState } from 'src/app/store/state/app.state';
 })
 export class AddBlogComponent implements OnInit {
   fileData: File = null;
-  previewUrl:any = null;
-  blogForm;
-  // fileUploadProgress: string = null;
+  previewUrl:any = null;/**to hold the url of the preview*/
+  blogForm;  /**to hold the blog form*/
+
   uploadedFilePath: string = null;
 
   constructor(
+    /**inject the formBuilder*/
     private formBuilder: FormBuilder,
+    /**inject the store*/
     private _store: Store<IAppState>
   ) { 
-
+    /**the form for the blog*/
     this.blogForm = this.formBuilder.group({
       title: "",
       body: "",
@@ -30,15 +32,18 @@ export class AddBlogComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  /**to monitor the upload of the file and add to preview*/
   fileProgress(event: any){
     this.fileData = <File> event.target.files[0];
     this.preview()
   }
+  /**To preview the uploaded file*/
   preview(){
     let reader = new FileReader()
+    /**Read the image uploaded*/
     reader.readAsDataURL(this.fileData);
     reader.onload = (_event) => { 
+      /**set the url to the previewUrl*/
       this.previewUrl = reader.result; 
     }
   }
@@ -46,13 +51,17 @@ export class AddBlogComponent implements OnInit {
     if (this.blogForm.invalid) {
       return;
     }
+    /**Dummy image data to display on the blog*/
     let dummyImagesUrl: string[] = [
       "https://lets-share.nyc3.digitaloceanspaces.com/chain-1812013_1280.jpg",
       "https://lets-share.nyc3.digitaloceanspaces.com/girl-1868930_1280.jpg",
       "https://lets-share.nyc3.digitaloceanspaces.com/wedding-rings-3611277_1280.jpg"
     ]
+    /**random number to pick a photo url from the url*/
     let randomNumber = Math.floor(Math.random() * 3 );
+    /**pick a random url*/
     let photoUrl =  dummyImagesUrl[randomNumber];
+    /**form the data to send as payload*/
     let data = {
       title: blogData.title,
       photoUrl: photoUrl,
@@ -60,6 +69,7 @@ export class AddBlogComponent implements OnInit {
       id: "10",
       commentNo: 4
     }
+    /**dispatch the createBlog action*/
     this._store.dispatch(new CreateBlog(data));
   }
 
