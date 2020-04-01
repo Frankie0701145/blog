15f93c25a2a0fdbbe0607380deaa94847b58ad92
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-01 03:09:25 +0300

    Fixed the error.

---
+++ src/app/models/successMessage.interface.ts
@@ -0,0 +1,5 @@
+
+export interface ISuccessMessage {
+    statusCode: string,
+    message: string
+}
--- src/app/store/state/successMessages.state.ts
+++
@@ -1,9 +0,0 @@
-import {ISuccessMessage} from '../../models/successMessage.interface'
-
-export interface ISuccessMessagesState {
-    successMessages: ISuccessMessage[]
-}
-
-export const initialSuccessMessagesState: ISuccessMessagesState ={
-    successMessages:[]
-}
