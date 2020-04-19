1a17c560e90e668e1c80bbf759ae6b4833aae2b0
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-19 20:22:47 +0300

    Remove the reducer, state and selector of the newBlogs

--- src/app/store/state/newBlog.state.ts
+++
@@ -1,9 +0,0 @@
-import {IBlog} from '../../models/blog.interface'
-
-export interface INewBlogsState {
-    newBlogs: IBlog[]
-} 
-
-export const initialINewBlogsState: INewBlogsState = {
-    newBlogs: []
-}
