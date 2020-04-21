f4ffdd68cd81abb72e1b5b3bfabf6c30f5e71b4a
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-21 08:57:20 +0300

    Define the success message selector

---
+++ src/app/store/selectors/successMessage.selector.ts
@@ -0,0 +1,11 @@
+/**For selecting the successMessage portion of the state*/
+import {createSelector} from '@ngrx/store';
+import { IAppState } from '../state/app.state';
+import {ISuccessMessageState} from '../state/successMessage.state'
+
+const selectSuccessMessage = (state: IAppState)=> state.successMessage
+
+export const selectSuccessMessageList = createSelector(
+    selectSuccessMessage,
+    (state: ISuccessMessageState)=> state.successMessage
+)
