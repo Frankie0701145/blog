5280fe0cf87e7caa465a938e3c32c0243a9ba3aa
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-22 13:27:19 +0300

    Add jsdoc to the filteredBlogs.reducer.

--- src/app/store/reducers/filteredBlogs.reducer.ts
+++ src/app/store/reducers/filteredBlogs.reducer.ts
@@ -1,32 +1,42 @@
-/**Filter the Blogs Reduce*/
+/**
+ * filteredBlogs reducer
+*/
 import {initialFilteredBlogsState, IFilteredBlogsState} from '../state/filteredBlogs.state';
 import {FilteredBlogActions, EFilteredBlogActions} from  '../actions/filteredBlogs.action';
 
+
+/**
+ * @param state 
+ * @param {FilteredBlogActions} action 
+*/
 export function filterBlogsReducer(state=initialFilteredBlogsState, action: FilteredBlogActions): IFilteredBlogsState{
     switch(action.type){
         /**Search the blogs*/
         case EFilteredBlogActions.SearchBlogs:
-                /**Retrieve the blogs passed as paylog */
+                /**Retrieve the blogs passed from the payload */
                 let blogs = action.payload.blogs
-                /**Retrieve the search text to be used*/
+                /**Retrieve the search text to be used for searching*/
                 let searchText = action.payload.searchText
                 /**iterate the blogs and filter blogs that match the search text*/
                 let filteredBlogs = blogs.filter((blog)=>{
                     /**Create a regular expression from the search text and making sure it is not case sensitive*/
                     let titleRegx =  new RegExp(searchText, 'i');
                     /**Check if the current blog matches the regular express*/
-                    let result = titleRegx.test(blog.title);
+                    let result: boolean = titleRegx.test(blog.title);
                     return result
                 })
+                /**Return the blogs that match the searchText */
                 return {
                     ...state,
                     filteredBlogs
                 }
+        /**Remove all the filtered blogs on the filteredBlogs state*/
         case EFilteredBlogActions.RemoveFilteredBlogs:
             return{
                 ...state,
                 filteredBlogs: []
             }
+        /**Return the initialState */
         default:
             return state;
     }
