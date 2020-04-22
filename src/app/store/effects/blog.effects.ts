d8d1cd556a5b7d71be592eca75baca0cd188a68c
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-23 00:58:20 +0300

    Fixed the edit of blogs so that blogs that are not the server can be edited also.

--- src/app/store/effects/blog.effects.ts
+++ src/app/store/effects/blog.effects.ts
@@ -1,15 +1,15 @@
 import { Injectable, } from "@angular/core";
-import {Effect, Actions,ofType} from '@ngrx/effects'
+import { Effect, Actions, ofType } from '@ngrx/effects'
 import { BlogService } from 'src/app/services/blog.service';
 import { IAppState } from '../state/app.state';
 import { Store } from '@ngrx/store';
-import {switchMap, map, tap} from 'rxjs/operators';
-import { 
-    GetBlogs, EBlogActions, GetBlogsSuccess, CreateBlogSuccess, CreateBlog, EditBlog, EditBlogSuccess 
+import { switchMap, map, tap, catchError } from 'rxjs/operators';
+import {
+    GetBlogs, EBlogActions, GetBlogsSuccess, CreateBlogSuccess, CreateBlog, EditBlog, EditBlogSuccess
 } from '../actions/blog.actions';
 import { of, Observable } from 'rxjs';
 import { IBlog } from 'src/app/models/blog.interface';
-import {Router } from '@angular/router'
+import { Router } from '@angular/router'
 import { StartLoading, StopLoading } from '../actions/loading.actions';
 import { IBlogEditProperties } from 'src/app/models/editBlog.interface';
 import { AddSuccessMessage } from '../actions/successMessage.action';
@@ -19,7 +19,7 @@ import { AddSuccessMessage } from '../actions/successMessage.action';
  * @class
 */
 @Injectable()
-export class BlogEffects{
+export class BlogEffects {
     /**
      * Fetching for blogs using the blogService 
      * @returns {Observable<GetBlogsSuccess>} - Returns an GetBlogsSuccess observable
@@ -28,12 +28,12 @@ export class BlogEffects{
     @Effect()
     getBlogs$: Observable<GetBlogsSuccess> = this._action$.pipe(
         ofType<GetBlogs>(EBlogActions.GetBlogs),
-        switchMap(()=> this._blogService.fetchBlogs()),
-        switchMap( (userHttp: any)=> {
+        switchMap(() => this._blogService.fetchBlogs()),
+        switchMap((userHttp: any) => {
             return of(new GetBlogsSuccess(userHttp))
-        } )
+        })
     )
-    
+
     /**
      * Posting new blogs using blogService
      * @returns {ObservableM<IBlog | CreateBlogSuccess>}
@@ -42,18 +42,18 @@ export class BlogEffects{
     postBlogs$: Observable<IBlog | CreateBlogSuccess> = this._action$.pipe(
         ofType<CreateBlog>(EBlogActions.CreateBlog),
         map(action => action),
-        switchMap((action)=>{
+        switchMap((action) => {
             /**Action to change the state loading to true*/
             this._store.dispatch(new StartLoading());
             /**Post post blogs to the server*/
             return this._blogService.postBlog(action.payload);
         }),
-        switchMap((blogHttp: IBlog)=>{
+        switchMap((blogHttp: IBlog) => {
             /**Add the success Message*/
-            this._store.dispatch(new AddSuccessMessage({message: 'Blog created Successful'}));
+            this._store.dispatch(new AddSuccessMessage({ message: 'Blog created Successful' }));
             return of(new CreateBlogSuccess(blogHttp))
         }),
-        tap(()=>{
+        tap(() => {
             /**Action to change the state loading to false*/
             this._store.dispatch(new StopLoading());
             /**Navigate the user to the blogs page*/
@@ -66,34 +66,37 @@ export class BlogEffects{
      * @return the observable EditBlogSuccess
     */
     @Effect()
-    editBlog$: Observable<EditBlogSuccess> = this._action$.pipe(
+    editBlog$: Observable<AddSuccessMessage | Observable<AddSuccessMessage>> = this._action$.pipe(
         ofType<EditBlog>(EBlogActions.EditBlog),
         map(action => action),
-        switchMap((action)=>{
+        switchMap((action) => {
             /**Action to change the state loading to true*/
             this._store.dispatch(new StartLoading());
-            return this._blogService.editBlog(
-                   action.payload.blogId, 
-                   action.payload.blogProperties
-                )
+            return this._blogService.editBlog(action.payload.blogId, action.payload.blogProperties).pipe(
+                map((blogHttp: IBlog) => {  
+                    this._store.dispatch(new EditBlogSuccess({
+                        blogId: blogHttp.id,
+                        blogProperties: {
+                            ...blogHttp
+                        }
+                    }));
+                    this._store.dispatch(new StopLoading());
+                    this.router.navigate(['/blogs']);
+                    return new AddSuccessMessage({ message: 'Blog edited successful' });
+                }),
+                catchError((error) => {
+                    this._store.dispatch(new EditBlogSuccess(
+                        {
+                            blogId: action.payload.blogId,
+                            blogProperties: action.payload.blogProperties
+                        }
+                    ));
+                    this._store.dispatch(new StopLoading());
+                    this.router.navigate(['/blogs']);
+                    return of(new AddSuccessMessage({ message: 'Blog edited successful'}))
+                })
+            )
         }),
-        switchMap((blogHttp: IBlog)=>{
-            let payload: {blogId: string, blogProperties: IBlogEditProperties} = {
-                blogId: blogHttp.id,
-                blogProperties: {
-                    ...blogHttp
-                }
-            }
-            /**Add the success Message*/
-            this._store.dispatch(new AddSuccessMessage({message: 'Blog edited successful'}));
-            return of(new EditBlogSuccess(payload))
-        }),
-        tap(()=>{
-            /**Action to change the state loading to false*/
-            this._store.dispatch(new StopLoading());
-            /**Navigate the user to the blogs page*/
-            return this.router.navigate(['/blogs']);
-        })
     )
     /**
      * Inject following BlogService, Action, store, and router
@@ -108,5 +111,5 @@ export class BlogEffects{
         private _action$: Actions,
         private _store: Store<IAppState>,
         private router: Router
-    ){}
+    ) { }
 }
