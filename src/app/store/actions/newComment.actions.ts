1871b02413d3bb576ae9d16052710746e9a2f385
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-22 10:54:46 +0300

    Add newComment.action doc.

--- src/app/store/actions/newComment.actions.ts
+++ src/app/store/actions/newComment.actions.ts
@@ -2,14 +2,26 @@
 import { Action } from '@ngrx/store';
 import { IComment } from 'src/app/models/comment.interface';
 
-/**The Add New Constant Names*/
+/**
+ * @enum {string}
+ * constants for newComments action
+*/
 export enum ENewCommentActions {
     AddNewComment = "[Comment] Add New Comment"
 }
+
+/**
+ * @class
+ * @implements {Action}
+ * Adds new comments to the newComments 
+*/
 
 export class AddNewComment implements Action {
     public readonly type = ENewCommentActions.AddNewComment
     constructor(public payload: IComment){}
 }
 
+/**
+ * the newComment type
+*/
 export type NewCommentActions = AddNewComment
