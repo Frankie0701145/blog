03b7e7fa87d2a30d052e1a8b0aee4195dbf648ae
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-22 09:15:50 +0300

    Adds the npm run script to generate jsdoc. Add documentation for the blog.actions

--- src/app/store/actions/blog.actions.ts
+++ src/app/store/actions/blog.actions.ts
@@ -1,10 +1,14 @@
-/**The Blog Action*/
+/**
+ * The Blog Actions
+*/
 
 import { Action } from '@ngrx/store';
 import {IBlog} from '../../models/blog.interface';
 import { IBlogEditProperties } from 'src/app/models/editBlog.interface';
 
-/**The action constant for the blog actions*/
+/**
+ * The constants for the blog actions
+*/
 export enum EBlogActions {
     GetBlogs = '[Blogs] Get Blogs',
     GetBlogsSuccess = '[Blogs] Get Blogs Success',
@@ -16,42 +20,111 @@ export enum EBlogActions {
     EditBlogSuccess = '[Blogs] Edit Blog Success'
 }
 
-/**The GetBlogs Action */
+/**
+ * @class
+ * @implements {Action}
+ * A GetBlogs Action class that implements Action
+ * The GetBlogs Action. Action for retrieving blogs from the server which will be handle by an effect.
+*/
 export class GetBlogs implements Action {
+    /**
+     * Holds the action type constant
+    */
     public readonly type = EBlogActions.GetBlogs
 }
 
-/** The GetBlogs Success*/
+/** 
+ * @class
+ * @implements {Action}
+ * A GetBlogsSuccess Action class that implements Action
+ * The GetBlogs Success. Action to be triggered after a successful GetBlogs Action.
+ * Adds the blog state
+*/
 export class GetBlogsSuccess implements Action {
+    /**
+     * Holds the action type constant
+    */
     public readonly type = EBlogActions.GetBlogsSuccess
+    /**
+     * @param {IBlog} payload - The payload contains blog properties and value
+    */ 
     constructor(public payload: IBlog[]){}
 }
 
-/** The CreateBlog Action*/
+/** 
+ * @class
+ * @implements {Action}
+ * A CreateBlog Action class that implements Action
+ * The CreateBlog Action. Action for creating blogs on the server which will be handled by an effect.
+*/
 export class CreateBlog implements Action {
+    /**
+     * Holds the action type constant
+    */
     public readonly type = EBlogActions.CreateBlog
+    /**
+     *@param {IBlog} payload - The payload contains blog properties and value
+    */
     constructor(public payload: IBlog){}
 }
 
-/** The CreateBlogSuccess Action*/
+/** 
+ * @class
+ * @implements {Action}
+ * A CreateBlogSuccess Action class that implements Action
+ * The CreateBlogSuccess Action. Action to be triggered after a successful CreateBlog.
+ * Adds a new blog to the blog state
+*/
 export class CreateBlogSuccess implements Action {
+    /**
+     * Holds the action type constant
+    */
     public readonly type = EBlogActions.CreateBlogSuccess
+    /**
+     *@param {IBlog} payload - The payload contains blog properties and value
+    */
     constructor(public payload: IBlog){}
 }
 
-/**Edit Blog Action*/
+/**
+ * @class
+ * @implements {Action}
+ * A EditBlog Action class that implements Action
+ * Edit Blog Action. Action for editing the blogs on the server which will be handled by an effect.
+*/
 export class EditBlog implements Action {
+    /**
+     * Holds the action type constant
+    */
     public readonly type = EBlogActions.EditBlog
+    /**
+     * @param {blogId: string, blogProperties: IBlogEditProperties} payload - The payload contains the blogId and  Properties and values that are going to change on a blog.
+     * @param {blogId: string, blogProperties: IBlogEditProperties} payload.blogId - The blogId to retrieve the blog that is to be edited
+     * @param {IBlogEditProperties} payload.blogProperties - Properties and values that are going to change on a blog.
+    */
     constructor( public payload: {blogId: string, blogProperties: IBlogEditProperties}){}
 }
 
-/** The EditBlogSuccess Action*/
+/** 
+ * @class
+ * @implements {Action}
+ * A EditBlogSuccess Action class that implements Action
+ * The EditBlogSuccess Action. Action to be triggered after a successful EditBlog.
+ * Edits the blogs in the state
+*/
 export class EditBlogSuccess implements Action {
     public readonly type = EBlogActions.EditBlogSuccess
+    /**
+     * @param {blogId: string, blogProperties: IBlogEditProperties} payload - The payload contains the blogId and  Properties and values that are going to change on a blog.
+     * @param {blogId: string, blogProperties: IBlogEditProperties} payload.blogId - The blogId to retrieve the blog that is to be edited
+     * @param {IBlogEditProperties} payload.blogProperties - Properties and values that are going to change on a blog.
+    */
     constructor( public payload: {blogId: string, blogProperties: IBlogEditProperties}){}
 }
 
 
-/** The Type for the BlogAction*/
+/** 
+ * The Type for the BlogAction.
+*/
 export type BlogActions = GetBlogs | GetBlogsSuccess | CreateBlog | CreateBlogSuccess | EditBlogSuccess;
 
