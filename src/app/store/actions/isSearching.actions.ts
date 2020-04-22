c57e6c7acec8c9e885c71669bcd5c2d310efca06
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-22 10:37:25 +0300

    Setup docs for isSearching.action

--- src/app/store/actions/isSearching.actions.ts
+++ src/app/store/actions/isSearching.actions.ts
@@ -1,20 +1,35 @@
 import { Action } from '@ngrx/store';
 
-/**The filteredBlogs constant names */
+/**The constant names for IsSearchActions */
 export enum EIsSearchingActions {
     Searching= '[Navbar] Searching',
     StopSearching = '[Navbar] Stop Searching',
-
 }
 
-/**Searching Action */
+/**
+ * @class
+ * @implements {Action}
+ * Searching Action.
+ * The action toggles the search state to true
+*/
 export class Searching implements Action {
+    /**
+     *  Holds the action type constant
+    */
     public readonly type = EIsSearchingActions.Searching
     
 }
 
-/**Stop Searching Action */
+/**
+ * @class
+ * @implements {Action}
+ * Stop Searching Action 
+ * The action toggles the search state to false
+*/
 export class StopSearching implements Action {
+    /**
+     *  Holds the action type constant
+    */
     public readonly type = EIsSearchingActions.StopSearching
 }
 
