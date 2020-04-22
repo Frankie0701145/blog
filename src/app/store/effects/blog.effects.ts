f24210eceb1c9cdd721aba1e73c5c1f2a5d0f199
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-22 11:28:01 +0300

    Add documentation for the blog.effects and comment.effect.

--- src/app/store/effects/blog.effects.ts
+++ src/app/store/effects/blog.effects.ts
@@ -7,49 +7,70 @@ import {switchMap, map, tap} from 'rxjs/operators';
 import { 
     GetBlogs, EBlogActions, GetBlogsSuccess, CreateBlogSuccess, CreateBlog, EditBlog, EditBlogSuccess 
 } from '../actions/blog.actions';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { IBlog } from 'src/app/models/blog.interface';
 import {Router } from '@angular/router'
 import { StartLoading, StopLoading } from '../actions/loading.actions';
 import { IBlogEditProperties } from 'src/app/models/editBlog.interface';
 import { AddSuccessMessage } from '../actions/successMessage.action';
 
+/**
+ * Injectable blogEffects class
+ * @class
+*/
 @Injectable()
 export class BlogEffects{
-    /**Fetching for blogs using the blogService */
+    /**
+     * Fetching for blogs using the blogService 
+     * @returns {Observable<GetBlogsSuccess>} - Returns an GetBlogsSuccess observable
+     * 
+    */
     @Effect()
-    getBlogs$ = this._action$.pipe(
+    getBlogs$: Observable<GetBlogsSuccess> = this._action$.pipe(
         ofType<GetBlogs>(EBlogActions.GetBlogs),
         switchMap(()=> this._blogService.fetchBlogs()),
         switchMap( (userHttp: any)=> {
             return of(new GetBlogsSuccess(userHttp))
         } )
     )
-    /**Posting new blogs using blogService*/
+    
+    /**
+     * Posting new blogs using blogService
+     * @returns {ObservableM<IBlog | CreateBlogSuccess>}
+    */
     @Effect()
-    postBlogs$ = this._action$.pipe(
+    postBlogs$: Observable<IBlog | CreateBlogSuccess> = this._action$.pipe(
         ofType<CreateBlog>(EBlogActions.CreateBlog),
         map(action => action),
         switchMap((action)=>{
+            /**Action to change the state loading to true*/
             this._store.dispatch(new StartLoading());
+            /**Post post blogs to the server*/
             return this._blogService.postBlog(action.payload);
         }),
         switchMap((blogHttp: IBlog)=>{
+            /**Add the success Message*/
             this._store.dispatch(new AddSuccessMessage({message: 'Blog created Successful'}));
             return of(new CreateBlogSuccess(blogHttp))
         }),
         tap(()=>{
+            /**Action to change the state loading to false*/
             this._store.dispatch(new StopLoading());
+            /**Navigate the user to the blogs page*/
             return this.router.navigate(['/blogs']);
         })
     )
 
-    /*Edit the blog*/
+    /**
+     * Edit the blogs using blogService
+     * @return the observable EditBlogSuccess
+    */
     @Effect()
-    editBlog$ = this._action$.pipe(
+    editBlog$: Observable<EditBlogSuccess> = this._action$.pipe(
         ofType<EditBlog>(EBlogActions.EditBlog),
         map(action => action),
         switchMap((action)=>{
+            /**Action to change the state loading to true*/
             this._store.dispatch(new StartLoading());
             return this._blogService.editBlog(
                    action.payload.blogId, 
@@ -63,15 +84,25 @@ export class BlogEffects{
                     ...blogHttp
                 }
             }
+            /**Add the success Message*/
             this._store.dispatch(new AddSuccessMessage({message: 'Blog edited successful'}));
             return of(new EditBlogSuccess(payload))
         }),
         tap(()=>{
+            /**Action to change the state loading to false*/
             this._store.dispatch(new StopLoading());
+            /**Navigate the user to the blogs page*/
             return this.router.navigate(['/blogs']);
         })
     )
-
+    /**
+     * Inject following BlogService, Action, store, and router
+     * @constructor
+     * @param {BlogService} _blogService - Contains http calls for fetching blogs, editing blogs and posting blogs
+     * @param {Actions} _action$
+     * @param {Store} _store
+     * @param {Router} router 
+    */
     constructor(
         private _blogService: BlogService,
         private _action$: Actions,
--- src/app/store/effects/comment.effects.ts
+++ src/app/store/effects/comment.effects.ts
@@ -3,13 +3,21 @@ import { Effect, Actions, ofType } from '@ngrx/effects';
 import { GetComments, ECommentActions, GetCommentsSuccess } from '../actions/comment.actions';
 import { map, switchMap } from 'rxjs/operators';
 import {CommentService} from '../../services/comment.service'
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { IComment } from 'src/app/models/comment.interface';
 
+/**
+ * Injectable CommentEffects class
+ * @class
+*/
 @Injectable()
 export class CommentEffects{
+    /**
+     * Fetching for comments of a blog
+     * @return {Observable<GetCommentsSuccess>}
+    */
     @Effect()
-    getComments = this._action$.pipe(
+    getComments$: Observable<GetCommentsSuccess> = this._action$.pipe(
         ofType<GetComments>(ECommentActions.GetComments),
         map(action => {
             return action
@@ -19,7 +27,11 @@ export class CommentEffects{
             return of(new GetCommentsSuccess(comments))
         })
     )
-
+    /**
+     * Injects the Action and CommentService
+     * @param {Action} action
+     * @param {CommentService} _commentService - Contains http calls for fetching comments
+    */
     constructor(
         private _action$: Actions,
         private _commentService: CommentService,
