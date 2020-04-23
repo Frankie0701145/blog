af1d9c1c16f6a05845764ccba9c50318917a0cae
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-23 17:46:58 +0300

    c;ear

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,12 +11,24 @@ import { AddSuccessMessage } from 'src/app/store/actions/successMessage.action';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
+/**
+ * @class
+ * @implements {OnInit}
+*/
 export class LoginComponent implements OnInit {
-  /**The login form*/
+  /**
+   * The login form
+  */
   loginForm;
-  /**The property to toggle the visibility of the password input*/ 
+  /**
+   * The property to toggle the visibility of the password input.
+  */ 
   hide: boolean= true;
-
+  /**
+   * @param {FormBuilder} formBuilder
+   * @param {Store} _store
+   * @param {Router} route
+  */
   constructor(
     /**inject the formBuilder*/
     private formBuilder: FormBuilder,
@@ -25,7 +37,9 @@ export class LoginComponent implements OnInit {
     /**inject the router*/
     private route: Router
   ) { 
-    /**create the formBuilder*/
+    /**
+     * Initialize a login form group
+    */
     this.loginForm = this.formBuilder.group({
       email: ["", Validators.required],
       password: ["",Validators.required]
@@ -35,20 +49,32 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(userData){
+  /**
+   * Handles the submission of the login form
+   * @param formValue - login form value
+  */
+  onSubmit(formValue){
       /**Return if the form is invalid*/
       if (this.loginForm.invalid) {
           return;
       }
-      /**Payload to pass to the LoginSuccess Action*/
+      /**
+       * Payload to pass to the LoginSuccess Action
+      */
       let payload ={
         accessToken: "asmcdaklsnmclamslcmkalmc"
       }
-      /**Call the LoginSuccess action*/
+      /**
+       * Call the LoginSuccess action. Toggle the loggedIn state to true
+      */
       this._store.dispatch(new LoginSuccess(payload));
-      /**Dispatch the success message*/
+      /**
+       * Dispatch the AddSuccessMessage action.
+      */
       this._store.dispatch(new AddSuccessMessage({message:"Login in Successfully"}));
-      /**navigate to the blogs route*/
+      /**
+       * navigate to the blogs route.
+      */
       this.route.navigate(['/blogs']);
   }
 
