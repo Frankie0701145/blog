d66572b897b502779cf2915df22158ff9f2fa836
Francis Njuguna <coulsorfrancois@gmail.com>
2020-04-23 18:32:07 +0300

    Add the function of retrieving all comments on the comment.service.ts

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -6,19 +6,21 @@ import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-/**Service of fetching comments from the server*/
+/**
+ * Service of fetching comments from the server*/
 export class CommentService {
   /**base url*/
   Url: string= `${environment.baseUrl}/comments`
   
-  "https://my-json-server.typicode.com/Frankie0701145/blog-data/comments"
   constructor(
     /**inject the HttpClient*/
     private http: HttpClient
   ) { }
-  /**fetch comments */
-  fetchComments(blogId: string): Observable<Object>{
-      return this.http.get(this.Url)
+  /**
+   * Fetch all the comments.
+   * @return {Array<IBLog>} - Returns blogs
+  */
+  fetchAllComments(): Observable<Object>{
+    return this.http.get(this.Url)
   }
-
 }
